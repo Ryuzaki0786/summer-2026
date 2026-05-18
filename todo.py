bcc57746eac56a21bad2fa8c5bae9abc0b175db9
Ryuzaki0786 <ekamvi.x.mmv@gmail.com>
2026-05-18 09:06:33 +0530

    Plan of action

---
+++ todo.py
@@ -0,0 +1,4 @@
+# What's the data structure for a single todo? Stack
+# How am I parsing the command line argument (add/list/done/remove)?
+# Where does the JSON file live?
+# What happens if the file doesn't exist yet?
