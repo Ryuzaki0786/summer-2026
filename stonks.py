eaac52a9c198983803f555a9e44ec3e61e4f36ea
Ryuzaki0786 <ekamvi.x.mmv@gmail.com>
2026-05-19 12:32:37 +0530

    day 2: stock price CLI using Finnhub API

---
+++ stonks.py
@@ -0,0 +1,22 @@
+import requests
+import sys
+import json
+
+
+API_KEY = "d85vnfpr01qmuqqbota0d85vnfpr01qmuqqbotag"
+
+#URL = f"https://finnhub.io/api/v1/quote?symbol=AAPL&token={API_KEY}"
+
+def display(Tickers):
+    
+    for T in Tickers:
+        URL = f"https://finnhub.io/api/v1/quote?symbol={T}&token={API_KEY}"
+        response = requests.get(URL)
+        data = response.json()
+        print(f"{T} ${data['c']}")
+    pass
+
+if __name__ == "__main__":
+    if(len(sys.argv) < 4):
+        sys.exit(1)
+    display(sys.argv[2:])
