4129a24a7038b68793e86ab04d18c9f0fd409122
Ryuzaki0786 <ekamvi.x.mmv@gmail.com>
2026-05-30 18:11:31 +0530

    chatroom: add README with architecture docs

--- chatroom/server.c
+++ chatroom/server.c
@@ -26,24 +26,76 @@ int main()
     listen(server_fd, 5);
     printf("Server listening on port 9090...\n");
 
-    //Accept a client
-    int client_fd =  accept(server_fd,NULL,NULL);
-    printf("Client connected: %d\n",client_fd);
+    
 
-    //Echo loop - read from client, send it back
-    char buffer[256];
+    int clients[64];
+    int client_count = 0;
+    
+    /*
+            select() wakes up
+            → New connection? Accept it, add to array
+            → For each client: did they send something?
+            → Yes and data > 0: broadcast to everyone else
+            → Yes and data = 0: they left, remove them
+            → No: skip, they're idle
+            → Back to select(), wait again
+    */
     while(1)
     {
-        ssize_t Bytes_read = read(client_fd,buffer,256);
-        
-        if(Bytes_read == 0)
-        {
-            printf("The client disconnected");
-            break;
-        }
-        buffer[Bytes_read] = '\0'; 
-        printf("Received: %s", buffer);
+        fd_set read_fds;
 
-        write(client_fd,buffer,Bytes_read);
+        FD_ZERO(&read_fds);
+        FD_SET(server_fd,&read_fds);
+
+        int max_fd = server_fd;
+
+        for(int i = 0; i< client_count;i++)
+        {
+            FD_SET(clients[i],&read_fds);
+            if(clients[i] > max_fd)
+            {
+                max_fd = clients[i];
+            }
+        }
+
+        select(max_fd + 1, &read_fds,NULL,NULL,NULL);
+        if(FD_ISSET(server_fd,&read_fds))
+        {
+            int new_client =  accept(server_fd,NULL,NULL);
+            clients[client_count++] = new_client;
+            printf("New client connected! Total: %d\n", client_count);
+        }
+        
+        for(int i = 0; i< client_count;i++)
+        {
+            if(FD_ISSET(clients[i],&read_fds))
+            {
+
+                char buffer[256];
+                int n = recv(clients[i],buffer,sizeof(buffer),0);
+
+                if(n <= 0)
+                {
+                    close(clients[i]);
+                    clients[i] = clients[--client_count];
+                }
+                else
+                {
+                    buffer[n] = '\0';
+                    printf("Received: %s",buffer);
+
+                    for(int j = 0; j<client_count;j++)
+                    {
+                        if(j != i)
+                        {
+                            write(clients[j],buffer,n);
+                        }
+                        
+                    }
+                }
+            }
+        }
+
+
     }
 }
