2710467d611afb28db1d5eb2fb513bd44d081d1c
Ryuzaki0786 <ekamvi.x.mmv@gmail.com>
2026-06-26 17:26:30 +0530

    Added untracked files

--- quantum-sim/tests/plot_schrodinger.py
+++ quantum-sim/tests/plot_schrodinger.py
@@ -8,6 +8,15 @@ n_points = data.shape[1]
 dx = length / (n_points - 1)
 x = np.linspace(0, length, n_points)
 
+center = 0.5            # whatever you set
+width  = 0.02           # whatever you set
+barrier_right = center + width / 2.0
+
+past  = x > barrier_right
+final = data[-1]
+T = np.sum(final[past]) / np.sum(final)
+print(f"Transmission coefficient T = {T:.6e}")
+
 # --- Check 1 & 2: motion + spreading (visual) ---
 plt.figure(figsize=(10, 6))
 n_snaps = data.shape[0]
